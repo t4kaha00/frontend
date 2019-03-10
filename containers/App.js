f0395bfc44b48f4b6cb4f6b92cf5445d4361790b
Harjit Karmacharya <hkarmacharya@gmail.com>
2019-03-10 04:36:11 +0200

    initial commits

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,50 @@
 import React, {Component} from 'react';
+import { Navbar, Nav, NavItem, NavDropdown, NavbarBrand } from 'react-bootstrap';
+import DropdownToggle from 'react-bootstrap/DropdownToggle';
+import DropdownMenu from 'react-bootstrap/DropdownMenu';
+import DropdownItem from 'react-bootstrap/DropdownItem';
+import NavbarCollapse from 'react-bootstrap/NavbarCollapse';
 
 export default class App extends Component {
     render () {
-        return <p>This is my new react app</p>
+        return (
+        <div>
+            <p>This is my new react app</p>
+            <p>Second line.</p>
+
+            <Navbar>
+            <NavbarBrand>
+                <strong>Liana</strong>
+            </NavbarBrand>
+        
+            <Nav>
+                <NavItem>Home</NavItem>
+                <NavItem>News</NavItem>
+                <NavItem>Company</NavItem>
+                <NavItem>Contact Us</NavItem>
+            </Nav>
+            <NavDropdown title="Language">
+                <DropdownMenu>
+                    <DropdownItem>Finnish</DropdownItem>
+                    <DropdownItem>English</DropdownItem>
+                </DropdownMenu>
+            </NavDropdown>            
+            </Navbar>
+
+            {/* <Navbar bg="dark" expand="lg">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic.navbar-nav">
+                <NavLink href="#news">News</NavLink>
+                <NavLink href="#intranet">Intranet</NavLink>
+                <NavDropdown title="Dropdown" id="language-dropdown">
+                <NavDropdown.Item href="finnish">Finnish</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="english">English</NavDropdown.Item>
+                </NavDropdown> 
+                </Navbar.Collapse>
+            </Navbar> */}
+        
+        </div>
+        )
     }
 }
