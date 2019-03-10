4902f05c6808977524198a00cb2265df8b3a9762
Harjit Karmacharya <hkarmacharya@gmail.com>
2019-03-10 20:01:31 +0200

    News section added

--- containers/App.js
+++ containers/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Navbar, Nav, NavItem, NavDropdown, NavbarBrand, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown, NavbarBrand, FormControl, Button, Figure, Card, CardGroup, CardDeck, Jumbotron } from 'react-bootstrap';
 import DropdownToggle from 'react-bootstrap/DropdownToggle';
 import DropdownMenu from 'react-bootstrap/DropdownMenu';
 import DropdownItem from 'react-bootstrap/DropdownItem';
@@ -12,7 +12,7 @@ export default class App extends Component {
     render () {
         return (
         <div>
-            <Navbar bg="light" expand="lg">
+            <Navbar className="bg-success" expand="lg">
                 <NavLink>News</NavLink>
                 <NavLink>Intranet</NavLink>
                 <NavDropdown title="Language" id="basic-nav-dropdown">
@@ -23,7 +23,7 @@ export default class App extends Component {
 
             {/* <p>This is my new react app</p> */}
 
-            <Navbar bg="primary" expand="lg">
+            <Navbar bg="success" expand="lg">
             <NavbarBrand >
                 <strong>Liana</strong>
             </NavbarBrand>
@@ -42,18 +42,72 @@ export default class App extends Component {
             </Navbar.Collapse>
             </Navbar>
 
-            {/* <Navbar bg="dark" expand="lg">
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic.navbar-nav">
-                <NavLink href="#news">News</NavLink>
-                <NavLink href="#intranet">Intranet</NavLink>
-                <NavDropdown title="Dropdown" id="language-dropdown">
-                <NavDropdown.Item href="finnish">Finnish</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="english">English</NavDropdown.Item>
-                </NavDropdown> 
-                </Navbar.Collapse>
-            </Navbar> */}
+            <Card className="bg-dark" text="light">
+                <Card.Img src="../src/together.jpg" alt="Card image" thumbnail/>
+                <Card.ImgOverlay className="text-center ">
+                    <Card.Title>Software fueling digital marketing</Card.Title>
+                    <Button variant="success">Learn More</Button>
+                </Card.ImgOverlay>
+            </Card>
+            
+            <CardDeck className="p-4">
+                <Card className="text-center" text="light" >
+                <Card.Img src="../src/company.jpg" alt="Card image"  />
+                <Card.ImgOverlay>
+                    <Card.Title className="card-body ">Company</Card.Title>
+                </Card.ImgOverlay>
+                </Card>
+                <Card className="text-center" text="light">
+                <Card.Img src="../src/together.jpg" alt="Card image" />
+                <Card.ImgOverlay>
+                    <Card.Title className="card-body mb-0">Products</Card.Title>
+                </Card.ImgOverlay>
+                </Card>
+                <Card className="text-center" text="light">
+                <Card.Img src="../src/contact.jpg" alt="Card image" />
+                <Card.ImgOverlay>
+                    <Card.Title className="card-body mb-0">Contact Us</Card.Title>
+                </Card.ImgOverlay>
+                </Card>
+            </CardDeck>
+
+            <Jumbotron className="text-center p-4 ">
+            <h4>Latest News</h4>
+            <CardDeck bg="light" className="pm-8 text-center">
+                <Card className="p-4 text-left" style={{ width: '18rem' }}>
+                    <small className="text-muted mb-2">
+                        27.07.2016
+                    </small>
+                    <Card.Text>
+                        Liana Technologies and Encode Solutions merge to create unique mobile solutions
+                    </Card.Text>
+                </Card>
+                <Card className="p-4 text-left" style={{ width: '18rem' }}>
+                    <small className="text-muted mb-2">
+                        28.01.2016
+                    </small>
+                    <Card.Text>
+                        From a local startup to a global player: Liana Technologies among the forerunners of digital marketing technology 
+                    </Card.Text>
+                </Card>
+                <Card className="p-4 text-left" style={{ width: '18rem' }}>
+                    <small className="text-muted mb-2">
+                        02.07.2015
+                    </small>
+                    <Card.Text>
+                        Liana Technologies Hong Kong launched a series od digital marketing events in June
+                    </Card.Text>
+                </Card>
+            </CardDeck>
+            </Jumbotron>
+
+
+           {/* <Figure>
+               <Figure.Image width={171} height={180} alt="171x180" src="../src/together.jpg" />
+               <Figure.Caption>
+                   Software fueling digital marketing
+               </Figure.Caption>
+           </Figure> */}
         
         </div>
         )
